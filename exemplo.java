f69d74e837c7c32abdec70ff344cc6f2842fb229
Liojes Carneiro <liojes@ifba.edu.br>
2026-04-15 19:33:38 +0000

    criar o Ola Mundo

--- exemplo.java
+++
@@ -1,4 +0,0 @@
-void main() {
-    String nome = IO.readln("Qual o seu nome, futuro dev? ");
-    IO.println("Olá, " + nome + "! Bem-vindo ao Java 25.");
-}
--- src/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo! Java 25 funcionando!");
-    }
-}
